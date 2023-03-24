205e0621b17092b4afad362da5605df18b8c3e96
zen <zhangymb@adtec.com.cn>
2023-03-24 23:33:40 +0800

    每次运行删除日志

--- log.go
+++ log.go
@@ -15,12 +15,20 @@ var (
 
 func init() {
 	file := "program.log"
+	if FileExist(file){
+		os.Remove(file)
+	}
 	logf, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Println("打开日志文件错误")
 	}
+	defer logf.Close()//如果不关闭可能造成内存泄露
 	Info = log.New(os.Stdout, "INFO:", log.Ltime)
 	Debug = log.New(io.MultiWriter(logf, os.Stdout), "DEBUG:", log.Lshortfile)
 	Warn = log.New(io.MultiWriter(logf, os.Stdout), "WARN:", log.Ltime|log.Llongfile)
 	TTY = log.New(os.Stdout, "TTY:", 0)
+}
+func FileExist(path string) bool {
+	_, err := os.Lstat(path)
+	return !os.IsNotExist(err)
 }
